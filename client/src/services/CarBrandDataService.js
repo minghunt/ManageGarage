184bd4a38f107266ed3beed0802590ce25a03245
minghunt <114232628+minghunt@users.noreply.github.com>
2023-05-24 23:24:49 +0700

    add api rule

--- client/src/services/CarBrandDataService.js
+++ client/src/services/CarBrandDataService.js
@@ -3,7 +3,7 @@ import { BASE_URL } from "../config/config";
 
 class CarBrandDataService {
     getAllCars() {
-        return axios.get(`${BASE_URL}/api/cars`)
+        return axios.get(`${BASE_URL}/api/cars-brand`)
     }
     getCarById(id){
         return axios.get(`${BASE_URL}/api/cars/${id}`)
@@ -12,14 +12,14 @@ class CarBrandDataService {
         return axios.get(`${BASE_URL}/api/cars/name/${TenHieuXe}`)
     }
     createCar(TenHieuXe){
-        return axios.post(`${BASE_URL}/api/cars`, ({ TenHieuXe }))
+        return axios.post(`${BASE_URL}/api/cars-brand`, ({ TenHieuXe }))
     }
     updateCar(data){
         return axios.put(`${BASE_URL}/api/cars`,data)
     }
     // Xóa HieuXe bằng MaHieuXe
     deleteCar(MaHieuXe) {
-        return axios.delete(`${BASE_URL}/api/cars/${MaHieuXe}`)
+        return axios.delete(`${BASE_URL}/api/cars-brand/${MaHieuXe}`)
     }
 }
 
