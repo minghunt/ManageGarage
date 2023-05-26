f495edf402d1ec8230f48ce50f8398ab768ae50f
minghunt <114232628+minghunt@users.noreply.github.com>
2023-05-26 21:59:36 +0700

    add interface ApplianceReceive

--- client/src/components/Task/ApplianceReceive/ApplianceReive.js
+++ client/src/components/Task/ApplianceReceive/ApplianceReive.js
@@ -1,10 +1,228 @@
-import React from "react";
-const ApplianceReive= ()=>{
+import React, { useEffect } from "react";
+import { useState } from 'react';
+import Col from "react-bootstrap/esm/Col";
+import Container from "react-bootstrap/esm/Container";
+import Row from "react-bootstrap/esm/Row";
+import Form from 'react-bootstrap/esm/Form'
+import Button from "react-bootstrap/Button";
+import FormGroup from "react-bootstrap/esm/FormGroup";
+import { MdLibraryAdd, MdDeleteForever } from 'react-icons/md'
+import PhuTungDataService from '../../../services/PhuTungDataService'
+const ApplianceReceive = () => {
+    const [reload, setReload] = useState(false)
+    const [AppliList, setAppliList] = useState([])
+    const [parts, setParts] = useState([{ name: null, quantity: null, price: 610000, total: 0 }])
+    const [listPhuTung, setListPhuTung] = useState([])
+    const [totalPrice, setTotalPrice] = useState(0)
+    const [NgayNhan, setNgayNhan] = useState(Date)
+    const [validated, setValidated] = useState(false);
+    const handleClickOpenAdd = (e) => {
+        let _parts = parts;
+        _parts.push({ name: null, quantity: null, price: AppliList[0].DonGia, total: 0 })
+        setParts(_parts)
+        setReload(!reload)
+    }
+
+    const handleDateChange = (e) => {
+        let _date = new Date(e.target.value)
+        setNgayNhan(_date)
+    }
+    const handleNameChange = (index, event) => {
+        let _parts = parts;
+        _parts[index].name = listPhuTung[event.target.value].TenPhuTung;
+        _parts[index].price = listPhuTung[event.target.value].DonGia;
+        _parts[index].total = _parts[index].quantity * _parts[index].price;
+        setParts(_parts)
+        setReload(!reload)
+    }
+    const handleInputChange = (index, event) => {
+        let _parts = parts;
+        _parts[index].quantity = event.target.value;
+        _parts[index].total = _parts[index].quantity * _parts[index].price;
+        setParts(_parts)
+        setReload(!reload)
+    }
+    const handlePriceChange = (index, event) => {
+        let _parts = parts;
+        _parts[index].price = event.target.value;
+        _parts[index].total = _parts[index].quantity * _parts[index].price;
+        setParts(_parts)
+        setReload(!reload)
+    }
+    const handleRemovePart = (index, event) => {
+        let _part = parts;
+        console.log('index', index)
+        _part.splice(index, 1)
+        setParts(_part)
+        setReload(!reload)
+    }
+    const handleSubmit = (event) => {
+        const form = event.currentTarget;
+        if (form.checkValidity() === false) {
+            event.preventDefault();
+            event.stopPropagation();
+        }
+        else {
+
+        }
+        setValidated(true);
+    };
+    useEffect(() => {
+        let s = 0;
+        parts.map(item => s += item.total)
+        setTotalPrice(s)
+        PhuTungDataService.getAllPhuTung()
+            .then((data) => {
+                setListPhuTung(data.data)
+            })
+        PhuTungDataService.getAllPhuTung()
+            .then((data) => {
+                setAppliList(data.data)
+            })
+    }, [parts, reload])
     return (
         <div>
+            <Container>
+                <Row>
+                    <Col xs='6'>
+                        <h2>
+                            Lập phiếu nhập VTPT
+                        </h2>
+                        <Form noValidate validated={validated} onSubmit={handleSubmit}>
+                            <Row className="mb-3">
+                                <Form.Group as={Col} md="6" controlId="validationCustom04">
+                                    <Form.Label>Ngày nhập vật tư</Form.Label>
+                                    <Form.Control type="date" placeholder="Chọn ngày" onChange={handleDateChange} name="begin" required />
+                                    <Form.Control.Feedback type="invalid">
+                                        Vui lòng chọn ngày tiếp nhận
+                                    </Form.Control.Feedback>
+                                </Form.Group>
+                                <Form.Group as={Col} md="6" controlId="validationCustom03">
+                                    <Form.Label>Tổng tiền</Form.Label>
+                                    <Form.Control type="text" value={totalPrice.toLocaleString('vi', { style: 'currency', currency: 'VND' })} readOnly />
+                                </Form.Group>
+                            </Row>
+                            <Row className="mb-3">
+                                <FormGroup>
+                                    Vật tư, phụ tùng:
+                                    <Button onClick={handleClickOpenAdd} style={{ marginLeft: '5px', border: 'none', width: '20%', textTransform: 'none', backgroundColor: '#0c828f', color: 'white' }}>
+                                        Thêm
+                                        <MdLibraryAdd style={{ marginLeft: '6px', fontSize: '18px' }} />
+                                    </Button>
+                                </FormGroup>
+                            </Row>
+                            <Form.Group controlId="parts" style={{ marginBottom: "20px" }}>
+                                <div style={{ paddingRight: '10px', maxHeight: '360px', overflow: 'hidden', overflowY: 'visible' }}>
 
-            <h1>tiep nhan vtpt</h1>
+                                    <Row>
+                                        <Col md='3'>
+                                            Tên Phụ tùng
+                                        </Col>
+                                        <Col md='2'>
+                                            Số lượng
+                                        </Col>
+                                        <Col md='3'>
+                                            Đơn giá
+                                        </Col>
+                                        <Col md='3'>
+                                            Thành tiền
+                                        </Col>
+                                        <Col md='1'>
+                                            Xóa
+                                        </Col>
+                                    </Row>
+                                    {parts.map((part, index) => (<>
+                                        <Row className="mb-2" style={{ paddingRight: '0' }}>
+                                            <Col md={3}>
+                                                <Form.Control
+                                                    as="select"
+                                                    onChange={(event) => handleNameChange(index, event)}
+                                                    required
+                                                >
+                                                    {listPhuTung.map((phuTung, key) =>
+                                                        part.name === phuTung.TenPhuTung ?
+                                                            <option value={key} selected>
+                                                                {phuTung.TenPhuTung}
+                                                            </option>
+                                                            :<option value={key}>
+                                                                {phuTung.TenPhuTung}
+                                                            </option>
+                                                    )}
+                                                </Form.Control>
+                                            </Col>
+                                            <Col md={2}>
+                                                <Form.Control
+                                                    type="number"
+                                                    min="1"
+                                                    value={part.quantity}
+                                                    onChange={(event) => handleInputChange(index, event)}
+                                                    required
+                                                />
+                                            </Col>
+                                            <Col md={3}>
+                                                <Form.Control
+                                                    type="number"
+                                                    min="0"
+                                                    step="500"
+                                                    defaultValue={part.price}
+                                                    value={part.price}
+                                                    onChange={(event) => handlePriceChange(index, event)}
+                                                />
+                                            </Col>
+                                            <Col md={3}>
+                                                <Form.Control type="text"name="total"
+                                                    value={part.total.toLocaleString('vi', { style: 'currency', currency: 'VND' })}
+                                                    readOnly
+                                                />
+                                            </Col>
+                                            <Col md={1}>
+                                                <Form.Group>
+                                                    <MdDeleteForever className='CarBrand-detele-btn' style={{ fontSize: '30px', marginTop: '3px' }} onClick={() => handleRemovePart(index)} />
+                                                </Form.Group>
+                                            </Col>
+                                        </Row>
+                                    </>))}
+                                </div>
+                            </Form.Group>
+                            <Button style={{ backgroundColor: '#0c828f', border: 'none' }} type="submit">Lập phiếu tiếp nhận</Button>
+                        </Form>
+                    </Col>
+                    <Col xs='6' className='CarBrandList-container' style={{ marginTop: '-10px' }}>
+                        <h2>
+                            Danh sách vật tư, phụ tùng
+                        </h2>
+                        <Row style={{ textAlign: 'center', paddingBottom: '10px', fontWeight: '700', paddingRight: '22px' }}>
+                            <Col xs='1' style={{ paddingRight: '5px' }}>
+                                STT
+                            </Col>
+                            <Col xs='4'>
+                                Tên vật tư
+                            </Col>
+                            <Col xs='4'>
+                                Đơn giá
+                            </Col><Col xs='3'>
+                                Số lượng tồn
+                            </Col>
+                        </Row>
+                        <div style={{ maxHeight: "500px", overflow: "hidden", overflowY: 'visible', paddingRight: '5px' }}>
+                            {AppliList.map((item, key) => <Row style={{ textAlign: 'center', padding: '8xp 10px', lineHeight: '27px', borderBottom: 'black 0.5px solid' }}>
+                                <Col xs='1' style={{ borderLeft: 'black 0.5px solid', paddingRight: '5px' }}>
+                                    {key + 1}
+                                </Col>
+                                <Col xs='4' style={{ borderLeft: 'black 0.5px solid' }}>
+                                    {item.TenPhuTung}
+                                </Col><Col xs='4' style={{ borderLeft: 'black 0.5px solid' }}>
+                                    {item.DonGia.toLocaleString('vi', { style: 'currency', currency: 'VND' })}
+                                </Col>
+                                <Col xs='3' style={{ borderLeft: 'black 0.5px solid' }}>
+                                    {item.SoLuongTon}
+                                </Col>
+                            </Row>)}
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
         </div>
     )
 }
-export default ApplianceReive;
+export default ApplianceReceive;
--- client/src/components/Task/CarSearch/CarSearch.js
+++ client/src/components/Task/CarSearch/CarSearch.js
@@ -1,8 +1,177 @@
-import React from "react";
-const CarSearch= ()=>{
+import React, { useEffect } from "react";
+import { useState } from 'react';
+import CarsBrandDataService from '../../../services/CarBrandDataService'
+import Col from "react-bootstrap/esm/Col";
+import Container from "react-bootstrap/esm/Container";
+import Row from "react-bootstrap/esm/Row";
+import Form from 'react-bootstrap/esm/Form'
+import Button from "react-bootstrap/Button";
+import InputGroup from 'react-bootstrap/InputGroup';
+import CarDataService from "../../../services/CarDataService";
+import { format } from 'date-fns';
+import viLocale from 'date-fns/locale/vi';
+function formatDateToVN(date) {
+    let _date = new Date(date)
+    return format(_date, 'dd/MM/yyyy', { locale: viLocale });
+}
+const CarSearch = () => {
+    const [HxeList, setHxeList] = useState([])
+    const [CarList, setCarList] = useState([])
+    const [BienSo, setBienSo] = useState([])
+    const [TenKH, setTenKH] = useState('')
+    const [MaHieuXe, setMaHieuXe] = useState(0)
+    const [NgayNhan, setNgayNhan] = useState(Date)
+    const [DienThoai, setDienThoai] = useState([])
+
+    const handleBienSoChange = (e) => {
+        setBienSo(e.target.value)
+    }
+    const handleTenKHChange = (e) => {
+        setTenKH(e.target.value)
+    }
+    const handleHieuXeChange = (e) => {
+        setMaHieuXe(e.target.value)
+    }
+    const handleDienThoaiChange = (e) => {
+        setDienThoai(e.target.value)
+    }
+    const handleDateChange = (e) => {
+        let _date = new Date(e.target.value)
+        setNgayNhan(_date)
+    }
+    const handleSubmitg = () => {
+        let _Car = {
+            BienSo: BienSo,
+            TenKH: TenKH,
+            MaHieuXe: MaHieuXe,
+            DienThoai: DienThoai,
+            NgayNhan: NgayNhan
+        }
+        console.log(_Car)
+
+        CarDataService.getAllCar(TenKH, BienSo, MaHieuXe, DienThoai, NgayNhan)
+            .then((data) => {
+                setCarList(data.data)
+            })
+    };
+    useEffect(() => {
+        CarsBrandDataService.getAllCarBrands(TenKH, BienSo, TenKH, BienSo, MaHieuXe, DienThoai, NgayNhan)
+            .then((data) => {
+                setHxeList(data.data)
+            })
+        CarDataService.getAllCar()
+            .then((data) => {
+                setCarList(data.data)
+            })
+    }, [])
     return (
         <div>
-            <h1>tim xe</h1>
+            <Container > 
+                <Row>
+                    <Col xs='12'>
+                        <h2>
+                            Tìm kiếm xe
+                        </h2>
+                        <Form  >
+                            <Row >
+                                <Form.Group as={Col}  >
+                                    <Form.Label>Biển số xe</Form.Label>
+                                    <Form.Control type="text" placeholder="Nhập biển số xe" onChange={handleBienSoChange} />
+                                </Form.Group>
+                                <Form.Group as={Col} controlId="validationCustom04">
+                                    <Form.Label>Hiệu xe</Form.Label>
+                                    <Form.Control as="select" required style={{ maxHeight: '55px' }} onChange={handleHieuXeChange}>
+                                        <option value={0}>
+                                            Chọn hiệu xe
+                                        </option>
+                                        {HxeList.map((item) => <option value={item.MaHieuXe}>
+                                            {item.TenHieuXe}
+                                        </option>)}
+                                    </Form.Control>
+                                </Form.Group>
+                                <Form.Group className="mb-3" as={Col} controlId="validationCustom04">
+                                    <Form.Label>Ngày tiếp nhận</Form.Label>
+                                    <Form.Control type="date" placeholder="Chọn ngày" onChange={handleDateChange} />
+                                </Form.Group>
+                                <Form.Group className="mb-3" as={Col} controlId="validationCustom01">
+                                    <Form.Label>Tên khách hàng</Form.Label>
+                                    <Form.Control
+
+                                        type="text"
+                                        placeholder="Nhập tên khách hàng" onChange={handleTenKHChange}
+                                    />
+
+                                </Form.Group>
+                                <Form.Group className="mb-3" as={Col} controlId="validationCustom02">
+                                    <Form.Label>Số điện thoại</Form.Label>
+                                    <Form.Control
+                                        onChange={handleDienThoaiChange}
+                                        type="tel" pattern="[0]{1}[0-9]{9}"
+                                        placeholder="Nhập Số ĐT"
+                                    />
+                                </Form.Group>
+                            </Row>
+
+                        </Form>
+                        <Button onClick={handleSubmitg} style={{ backgroundColor: '#0c828f', border: 'none',marginBottom:'10px' }} type="submit">Tìm kiếm</Button>
+
+                    </Col>
+                    <Col xs='12' className='CarBrandList-container' style={{margin:'0px ',}} >
+                        <h2>
+                            Danh sách xe
+                        </h2>
+                        <Row style={{ textAlign: 'center', paddingBottom: '10px', fontWeight: '700', paddingRight: '6px' }}>
+                            <Col xs='1' style={{ paddingRight: '5px' }}>
+                                STT
+                            </Col>
+                            <Col xs='1'>
+                                Biển số
+                            </Col>
+                            <Col xs='2'>
+                                Hiệu xe
+                            </Col><Col xs='2'>
+                                Tên khách hàng
+                            </Col>
+                            <Col xs='2'>
+                                Số điện thoại
+                            </Col>
+
+                            <Col xs='2'>
+                                Tiền nợ
+                            </Col>
+                            <Col xs='2'>
+                                Ngày nhận
+                            </Col>
+                        </Row>
+                        <div style={{ maxHeight: "500px", overflow: "hidden", overflowY: 'visible', paddingRight: '5px' }}>
+                            {CarList.map((item, key) => <Row style={{ textAlign: 'center', padding: '8xp 10px', lineHeight: '27px', borderBottom: 'black 0.5px solid' }}>
+                                <Col xs='1' style={{ borderLeft: 'black 0.5px solid', paddingRight: '5px' }}>
+                                    {key + 1}
+                                </Col>
+                                <Col xs='1' style={{ borderLeft: 'black 0.5px solid' }}>
+                                    {item.BienSo}
+                                </Col><Col xs='2' style={{ borderLeft: 'black 0.5px solid' }}>
+                                    {item.HieuXe}
+                                </Col>
+                                <Col xs='2' style={{ borderLeft: 'black 0.5px solid' }}>
+                                    {item.TenKH}
+                                </Col>
+                                <Col xs='2' style={{ borderLeft: 'black 0.5px solid' }}>
+                                    {item.DienThoai}
+                                </Col>
+                                <Col xs='2' style={{ borderLeft: 'black 0.5px solid' }}>
+                                    {item.TienNo.toLocaleString('vi', { style: 'currency', currency: 'VND' })}
+                                </Col>
+
+                                <Col xs='2' style={{ borderLeft: 'black 0.5px solid' }}>
+                                    {formatDateToVN(item.NgayNhan)}
+
+                                </Col>
+                            </Row>)}
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
         </div>
     )
 }
