47d10324003f7390a38b4ffc6fe761e891934a3c
hau1605 <125997831+hau1605@users.noreply.github.com>
2023-06-12 22:29:10 +0700

    updateu serRole2 (#13)
    * fogotPass
    
    * update be user
    
    * update role
    
    * update role 2

--- client/src/App.js
+++ client/src/App.js
@@ -8,7 +8,7 @@ import Report from "./components/Report/Report";
 import User from './components/User/User';
 function App() {
   const isLoggedIn = useSelector((state) => state.user.isLoggedIn);
-  const isUserRoleAdmin = useSelector((state) => state.user.isUserRoleAdmin);
+  const userRole = useSelector((state) => state.user.userRole);
 
   return (
     <div className="App">
@@ -17,7 +17,7 @@ function App() {
         <Route path="/reset-password" element ={isLoggedIn ? <Navigate to={'/Task'} replace /> :<ResetPassword />} />
         <Route path="/Task" element ={!isLoggedIn ? <Navigate to={'/'} replace /> : <Task/>}/>
         <Route path="/Report" element ={!isLoggedIn ? <Navigate to={'/'} replace /> : <Report />} />
-        <Route path="/Rule" element ={(!isLoggedIn || !isUserRoleAdmin ) ? <Navigate to={'/'} replace /> : <Rule />} />
+        <Route path="/Rule" element ={(!isLoggedIn || userRole === "admin" ) ? <Navigate to={'/'} replace /> : <Rule />} />
         <Route path="/user" element ={!isLoggedIn ? <Navigate to={'/'} replace /> : <User />} />
       </Routes>
     </div>
